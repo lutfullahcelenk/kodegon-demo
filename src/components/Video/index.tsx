a8974d62e05421c5a1c41f853258abda17806474
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-08-04 14:35:58 +0300

    video is applied by iframe responsively

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import useResponsive from "../../hooks/useResponsive";
 
 const Video = () => {
+  const { xs } = useResponsive();
+
   return (
     <div className="max-w-screen-md sm:m-16 mb-16">
-      as
+      <iframe
+        width="100%"
+        height={xs ? "211": "511"}
+        src="https://www.youtube.com/embed/5EZquD8JXro"
+        title="Eurovision Song"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
